5cead73792f80616e635a8d75a569f4ea4101476
Zesheng_Li <zl311@duke.edu>
2022-06-21 23:39:35 -0400

    lc06212022

--- Array/34.py
+++ Array/34.py
@@ -1,18 +1,1 @@
-class Solution:
-    # requiement: O(log n)
-    # try double points
-    def searchRange(self, nums, target) -> int:
-        if len(nums) == 0:
-            return [-1,-1]
-        left = 0
-        right = len(nums) - 1
-        while left< right:
-            
-
-
-
-if __name__ =="__main__":
-    s = Solution()
-    nums = [1,1,1,2,2,3]
-    i = s.searchRange(nums, 2)
-    print(nums[:i])
+## look at Binary Search
